3838325dd4cbc18b439027910e3e60b41dac6e88
Vladimir K <vladimir.sdev@gmail.com>
2021-01-04 19:23:26 +0300

    Add picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
+interface Person {
+  name: string;
+  age: number;
+  pictureUrl: string;
+}
+
 export default function App() {
-  const [female, setFemale] = useState("");
+  const [female, setFemale] = useState({} as Person);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -11,7 +17,9 @@ export default function App() {
       .then((res) => res.json())
       .then((json) => json.results)
       .then((_person) => _person[0])
-      .then(({ name, dob }) => setFemale(`${name.first}, ${dob.age}`))
+      .then(({ name: { first }, dob: { age }, picture: { large } }) =>
+        setFemale({ name: first, age: age, pictureUrl: large })
+      )
       .catch(console.error);
     return () => {
       setIsLoading(false);
@@ -21,7 +29,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Tinder App</h1>
-      <p>{female}</p>
+      <img alt="female" src={female.pictureUrl} />
+      <div>
+        {female.name}, {female.age}
+      </div>
       <button disabled={isLoading} onClick={() => setIsLoading(true)}>
         Get Person
       </button>
