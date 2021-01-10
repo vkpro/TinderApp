090f0524fe6d2b0fdc30caf0f6ae73cfbcde50fb
Vladimir K <vladimir.sdev@gmail.com>
2021-01-10 19:48:56 +0300

    Add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
-import "./styles.css";
+// import "./styles.css";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary
+    }
+  })
+);
 
 interface Person {
   name: string;
@@ -14,6 +30,7 @@ export default function App() {
   const [male, setMale] = useState({} as Person);
   const [isFemaleLoading, setIsFemaleLoading] = useState(false);
   const [isMaleLoading, setIsMaleLoading] = useState(false);
+  const classes = useStyles();
 
   useEffect(() => {
     if (isFemaleLoading) return;
@@ -45,35 +62,58 @@ export default function App() {
     };
   }, [isMaleLoading]);
 
-  return (
-    <div className="App">
-      <h1>Tinder App</h1>
+  const result =
+    Math.abs(female.age - male.age) <= 10
+      ? "It's a Match!"
+      : "Find Matches with no more than 10 years age difference              ";
 
-      {[female, male].map((person) => {
-        return (
-          <>
-            <img alt={person.name} src={person.pictureUrl} />
-            <div>
-              {person.name}, {person.age},
-            </div>
-            <Button
-              variant="contained"
-              color={person.gender === "female" ? "secondary" : "primary"}
-              disabled={
-                person.gender === "female" ? isFemaleLoading : isMaleLoading
-              }
-              onClick={() =>
-                person.gender === "female"
-                  ? setIsFemaleLoading(true)
-                  : setIsMaleLoading(true)
-              }
-            >
-              Next {person.gender}
-            </Button>
-            <hr />
-          </>
-        );
-      })}
-    </div>
+  return (
+    <>
+      <div className={classes.root}>
+        <h1 style={{ textAlign: "center" }}>Tinder App</h1>
+        <Grid container spacing={3}>
+          {[female, male].map((person) => (
+            <Grid item xs>
+              <Paper className={classes.paper}>
+                <>
+                  {person.name ? (
+                    <>
+                      <img alt={person.name} src={person.pictureUrl} />
+                      <div>
+                        {person.name}, {person.age}
+                      </div>
+
+                      <Button
+                        variant="contained"
+                        color={
+                          person.gender === "female" ? "secondary" : "primary"
+                        }
+                        disabled={
+                          person.gender === "female"
+                            ? isFemaleLoading
+                            : isMaleLoading
+                        }
+                        onClick={() =>
+                          person.gender === "female"
+                            ? setIsFemaleLoading(true)
+                            : setIsMaleLoading(true)
+                        }
+                      >
+                        Next {person.gender}
+                      </Button>
+                    </>
+                  ) : (
+                    <div>"Loading a new person"</div>
+                  )}
+                </>
+              </Paper>
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <h2 style={{ textAlign: "center" }}>{result}</h2>
+          </Grid>
+        </Grid>
+      </div>
+    </>
   );
 }
